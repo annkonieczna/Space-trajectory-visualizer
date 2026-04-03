5df9b459995333c4a77266ac5d0c348e322014ce
Anna Konieczna <annkonieczna@student.agh.edu.pl>
2026-04-03 21:52:43 +0200

    Add dowloading logic to rest of the kernels

--- src/download_kernels.py
+++ src/download_kernels.py
@@ -2,9 +2,19 @@ from pathlib import Path
 from urllib.request import urlretrieve
 
 KERNELS = {
-    "naif0012.tls": "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/naif0012.tls",
-    "pck00011.tpc": "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/pck00011.tpc",
-    "de440s.bsp": "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/de440s.bsp",
+    #generic kernels: 
+    "naif0012.tls": "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/naif0012.tls", #used for time conversion
+    "pck00011.tpc": "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/pck00011.tpc", #planetary constant 
+    "de442s.bsp": "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/de442s.bsp", #ephemeris for vehicles, planets, satelites
+    "sat458.bsp": "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/satellites/sat458.bsp", # saturn system 
+    #kernels specifically for Cassini mission: 
+    "200128RU_SCPSE_17098_17126.bsp": "https://naif.jpl.nasa.gov/pub/naif/CASSINI/kernels/spk/200128RU_SCPSE_17098_17126.bsp",
+    "200128RU_SCPSE_17126_17158.bsp": "https://naif.jpl.nasa.gov/pub/naif/CASSINI/kernels/spk/200128RU_SCPSE_17126_17158.bsp",
+    "200128RU_SCPSE_17158_17177.bsp": "https://naif.jpl.nasa.gov/pub/naif/CASSINI/kernels/spk/200128RU_SCPSE_17158_17177.bsp",
+    "200128RU_SCPSE_17177_17208.bsp": "https://naif.jpl.nasa.gov/pub/naif/CASSINI/kernels/spk/200128RU_SCPSE_17177_17208.bsp",
+    "200128RU_SCPSE_17208_17235.bsp": "https://naif.jpl.nasa.gov/pub/naif/CASSINI/kernels/spk/200128RU_SCPSE_17208_17235.bsp",
+    "200128RU_SCPSE_17235_17258.bsp": "https://naif.jpl.nasa.gov/pub/naif/CASSINI/kernels/spk/200128RU_SCPSE_17235_17258.bsp",
+
 }
 
 def main():
