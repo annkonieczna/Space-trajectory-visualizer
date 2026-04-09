91760dcd92fbfd74abbc7d63f77c85e8f50a59d3
Anna Konieczna <annkonieczna@student.agh.edu.pl>
2026-04-09 17:45:41 +0200

    Add config file

---
+++ src/config.py
@@ -0,0 +1,18 @@
+#This file contains all of the constants neccessary for this project 
+
+
+#time settings 
+DEFAULT_START_UTC = "2017-04-22T00:00:00" #T as a seperator between date and an hour 
+DEFAULT_END_UTC = "2017-09-15T00:00:00"
+DEFAULT_STEP_IN_SECONDS = 3600
+
+#Reference frame 
+
+DEFAULT_FRAME = "J2000" # reference frame I'll be using most of the time, it's an inertial refernce frame 
+#so it's useful for calculating most of the 
+DEFAULT_ABCORR = "NONE" #the aberration correction we want to apply 
+#in my case I want to have the geometrical state of the object relative to the observer 
+
+CASSINI_NAME = "CASSINI"
+SATURN_NAME = "SATURN"
+TITAN_NAME = "TITAN"
