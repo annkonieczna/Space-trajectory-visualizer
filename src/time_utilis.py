bc5e651da2bc39baec550fd576ba8c575c008daf
Anna Konieczna <annkonieczna@student.agh.edu.pl>
2026-04-17 02:28:45 +0200

    add generate_et_range

--- src/time_utilis.py
+++ src/time_utilis.py
@@ -2,13 +2,24 @@ import spiceypy as spice
 import numpy as np 
 from typing import cast 
 
-#converts time given by the user to Ephemeris Time used by SPICE 
+
 def utc_to_et(utc_str: str) -> float: 
+    '''Converts time given by the user to Ephemeris Time used by SPICE '''
     return cast(float,spice.str2et(utc_str))
 
-#the oppsite conversion 
+
 def et_to_utc(et: float, format_str: str = "ISOC",  prec: int = 3) -> str: #ISOC - ISO Calendar (standard format)
+    '''the oppsite conversion '''
     return cast(str,spice.et2utc(et,format_str,prec))
+
+def generate_et_range(utc_start:str,utc_end:str,step_seconds:int): 
+    '''Generates range of time in ET format'''
+    start = utc_to_et(utc_start)
+    stop = utc_to_et(utc_end)
+    return np.arange(start,stop + step_seconds,step_seconds)
+
+
+
 
 
 
