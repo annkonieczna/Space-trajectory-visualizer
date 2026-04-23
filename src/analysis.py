e7c0c3b4f5e07859aa49dc1e841d32aac854c77f
Anna Konieczna <annkonieczna@student.agh.edu.pl>
2026-04-23 21:33:45 +0200

    Add function for building distance dataframe

--- src/analysis.py
+++ src/analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from typing import cast
+import numpy as np
 
 SECONDS_IN_A_DAY:float = 86400.0 # number of seconds in a day 
 
@@ -27,3 +28,23 @@ def find_global_max(df: pd.DataFrame, column: str = "distance_km") -> pd.Series:
     """
     idx = df[column].idxmax()
     return  cast(pd.Series,df.loc[idx]) 
+
+def build_distance_dataframe(df_a: pd.DataFrame,df_b:pd.DataFrame, label: str) -> pd.DataFrame:
+    '''Function that is given dwo dataframes and it calculates the distance between 
+    two objects in the same reference frame and are relative to the same observer  
+    data frame looks like: et utc x y z'''
+    coords_a = df_a[["x_km,y_km,z_km"]].to_numpy()
+    coords_b = df_b[["x_km,y_km,z_km"]].to_numpy()
+    distances = np.linalg.norm(coords_a-coords_b,axis=1) #calculates difference between vectors for every row
+    new_df = pd.DataFrame(
+        {
+        "et": df_a["et"],
+        "utc": df_a["utc"],
+        "distance_km": distances
+        }
+    ) 
+    new_df["pair"] = label 
+    return add_relative_days(new_df)
+
+
+
