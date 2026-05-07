c371ea13a1bb15d14145fe1cbc865da00c78def2
Anna Konieczna <annkonieczna@student.agh.edu.pl>
2026-05-07 20:19:34 +0200

    Add a getter function for a body's radii

--- src/bodies.py
+++ src/bodies.py
@@ -12,5 +12,20 @@ class BodyEllipsoid:
     radii_km: np.ndarray  # radii of the ellipsoid
 
 
+def get_body_radii(body_name: str) -> np.ndarray:
+    """
+    Returns triaxial body radii from SPICE kernel pool.
+    """
+    # bodvrd(BODYNM, ITEM, MAXN)
+    # Inputs:
+    # BODYNM - Body name
+    # ITEM - RADII - Item for which values are desired
+    # MAXN -  Maximum number of values that may be returned
+    # Outputs:
+    # _ - Number of values returned
+    # radii -  Values
+
+    _, radii = spice.bodvrd(body_name, "RADII", 3)
+    return np.array(radii, dtype=float)
 
 
