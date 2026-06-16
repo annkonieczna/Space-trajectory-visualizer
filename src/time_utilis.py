af2a4269888dee6715eeefdb682ff147762fd14c
Anna Konieczna <annkonieczna@student.agh.edu.pl>
2026-06-16 21:42:53 +0200

    Add safeguards to time file

--- src/time_utilis.py
+++ src/time_utilis.py
@@ -18,6 +18,13 @@ def et_to_utc(
 
 def generate_et_range(utc_start: str, utc_end: str, step_seconds: int) -> np.ndarray:
     """Generates range of time in ET format"""
+    if step_seconds <= 0:
+        raise ValueError("Step in seconds must be greater than zero.")
+
     start = utc_to_et(utc_start)
     stop = utc_to_et(utc_end)
+
+    if stop < start:
+        raise ValueError("End date must be later than or equal to start date.")
+
     return np.arange(start, stop + step_seconds, step_seconds)
