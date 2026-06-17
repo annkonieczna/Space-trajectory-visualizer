9e90f874c6e413555e277ba645a153bf7f11ba0c
Anna Konieczna <annkonieczna@student.agh.edu.pl>
2026-06-17 17:14:03 +0200

    Change spice loader in order to work no matter in which directory the project is started

--- src/spice_loader.py
+++ src/spice_loader.py
@@ -1,9 +1,19 @@
+import os
+from pathlib import Path
+
 import spiceypy as spice
 
-
-def load_kernels(meta_kernel_path):
-    spice.furnsh(str(meta_kernel_path))
+from src.config import PROJECT_ROOT
 
 
-def clear_kernels():
+def load_kernels(meta_kernel_path: Path) -> None:
+    current_dir = Path.cwd()
+    try:
+        os.chdir(PROJECT_ROOT)
+        spice.furnsh(str(meta_kernel_path.resolve()))
+    finally:
+        os.chdir(current_dir)
+
+
+def clear_kernels() -> None:
     spice.kclear()
